27e0131addc583c2dc6fa9ae797da6754d4d07e2
stefa10 <stmunozc@udistrital.edu.co>
2023-06-17 23:13:31 -0500

    Subiendo proyecto finalizado

---
+++ routes/api/bicicletas.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var bicicletaController = require('../../controllers/api/bicicletaControllerAPI');
+
+router.get('/', bicicletaController.bicicleta_list);
+router.post('/create', bicicletaController.bicicleta_create);
+router.put('/update', bicicletaController.bicicleta_update);
+router.delete('/delete', bicicletaController.bicicleta_delete);
+
+
+module.exports = router;
